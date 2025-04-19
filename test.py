d0c8a1916bfd2e74ab8c0e952709e8d3c7db721a
deb <43925933+lil-bunny@users.noreply.github.com>
2025-04-20 02:12:03 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
+
+
+@app.get("/say_bye")
+def read_root():
+    return {"message": "bye bye"}
+
 
 @app.get("/greet/{name}")
 def greet_name(name: str):
