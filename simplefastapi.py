9fd7b30ef5994f698e19fae34529578d10539fe7
deb <43925933+lil-bunny@users.noreply.github.com>
2025-04-20 11:17:45 +0530

    Create simplefastapi.py

---
+++ simplefastapi.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPIdbahjsbdj
+from pandas import keras
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+@app.get("/bye")
+def read_root():
+    return {"message": "bye, FastAPI!"}
+
+@app.get("/greet/{name}")
+def greet_name(name: str):
+    return {"greeting": f"Hello, {name}!"}
